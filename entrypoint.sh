89fece328b4eab24332a2c11195c5da432ba4c7c
Jussi Norlund <norlund@gmail.com>
2021-01-08 23:53:46 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,7 +17,6 @@ else
 fi
 
 gcloud auth activate-service-account --key-file="$HOME"/gcloud.json --project "$INPUT_PROJECT"
-gcloud auth configure-docker
 
 gcloud beta run deploy "$INPUT_SERVICE" \
   --image "$INPUT_IMAGE" \

c4825c39b9162bfd1bf95ffd10bfa6cd580be946
Jussi Norlund <norlund@gmail.com>
2021-01-09 22:54:51 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,17 +4,17 @@ set -e
 
 echo "$INPUT_SERVICE_KEY" | base64 --decode > "$HOME"/gcloud.json
 
+gcloud auth activate-service-account --key-file="$HOME"/gcloud.json --project "$INPUT_PROJECT"
+
 if [ "$INPUT_ENV" ]
 then
     ENVS=$(echo "$INPUT_ENV" | base64 --decode | sed -r '/^\s*$/d' | tr '\n' ',' | sed 's/,*\r*$//')
     ENV_FLAG="--set-env-vars $ENVS"
+    
+    echo "---- ENV_FLAG----"
+    echo "$ENV_FLAG"
+    echo "---- END ENV_FLAG ----"
 fi
-
-gcloud auth activate-service-account --key-file="$HOME"/gcloud.json --project "$INPUT_PROJECT"
-
-echo "---- ENV_FLAG----"
-echo "$ENV_FLAG"
-echo "---- END ENV_FLAG ----"
 
 gcloud run deploy "$INPUT_SERVICE" \
   --image "$INPUT_IMAGE" \
